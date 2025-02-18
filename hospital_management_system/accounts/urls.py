d622cffa38288f56bcbd9bc5d6d18d5f0568f788
Immanuel N <immanuelnjogu@students.tukenya.ac.ke>
2025-02-18 11:40:44 +0300

    changes

--- hospital_management_system/accounts/urls.py
+++
@@ -1,6 +0,0 @@
-from accounts.views import RegisterView
-from django.urls import path,include
-
-urlpatterns = [
-    path('api/auth/register/', RegisterView.as_view(), name='register'),
-]
--- hospital_management_system/doctors/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from .models import Doctor
-from .serializers import DoctorSerializer
-
-# Create your views here.
-class DoctorViewSet(viewsets.ModelViewSet):
-    queryset = Doctor.objects.all()
-    serializer_class = DoctorSerializer
